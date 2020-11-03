c7e5a907262536e3e02983c4f64003304323605e
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2020-11-03 09:06:22 -0300

    Create cl_framework.h

---
+++ cl_framework.h
@@ -0,0 +1,24 @@
+char *append(const char *s, char c) {
+    int len = strlen(s);
+    char buf[len+2];
+    strcpy(buf, s);
+    buf[len] = c;
+    buf[len + 1] = 0;
+    return strdup(buf);
+}
+
+int minus(int n, int n2) {
+  return n-n2;
+}
+
+int divide(int n, int n2) {
+  return n/n2;
+}
+
+int sum(int n, int n2) {
+  return n+n2;
+}
+
+int multiply(int n, int n2) {
+  return n*n2;
+}
